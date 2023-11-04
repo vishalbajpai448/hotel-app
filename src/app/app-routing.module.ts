f6aaf544beedfb73c6a95e0db759bfa82afd64a5
Your Name <you@example.com>
2023-11-04 22:20:11 +0530

    Reactive Form Component with Validation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationFormComponent } from './reservation/reservation-form/reservation-form.component';
+import { ReservationListComponent } from './reservation/reservation-list/reservation-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent},
+  {path: 'new', component:ReservationFormComponent},
+  {path: 'list', component:ReservationListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReservationFormComponent } from './reservation-form/reservation-form.component';
 import { ReservationListComponent } from './reservation-list/reservation-list.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -12,7 +13,9 @@ import { ReservationListComponent } from './reservation-list/reservation-list.co
     ReservationListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
   ]
 })
 export class ReservationModule { }

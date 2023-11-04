b5d31b79f8aeec3667bc49293e4ffabaaf528734
Your Name <you@example.com>
2023-11-04 22:28:17 +0530

    Service File

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Reservation } from '../models/reservation';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,36 @@ import { Injectable } from '@angular/core';
 export class ReservationService {
 
   constructor() { }
+
+  private reservations : Reservation[] = [];
+
+  //Crud
+
+  getReservations(): Reservation[]
+  {
+    return this.reservations;
+  }
+
+  getReservation(id: string): Reservation | undefined
+  {
+    return this.reservations.find(res => res.id === id);
+  }
+
+  addReservation(reservation: Reservation): void
+  {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation(id: string): void 
+  {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1);
+  }
+
+  updateReservation(updatedReservation: Reservation): void
+  {
+    let index = this.reservations.findIndex(res => res.id === updatedReservation.id);
+    this.reservations[index] = updatedReservation;
+
+  }
 }
